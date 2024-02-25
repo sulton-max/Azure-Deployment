82a991dfae4e9d4af5273753dd5dd34987e214f5
Sultonbek Rakhimov <sultonbek.rakhimov@gmail.com>
2024-02-25 10:38:04 +0500

    Small change ))

--- src/Demo.Pipeline.Backend/DemoPipeline.Api/Program.cs
+++ src/Demo.Pipeline.Backend/DemoPipeline.Api/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello deployment!");
+app.MapGet("/", () => "Hello containers!");
 
 app.Run();
